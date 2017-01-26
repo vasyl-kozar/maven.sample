9148a5163d6e091244aa806b4c3a17c66c70a757
vasyl-kozar <kozar.vasyl@gmail.com>
2017-01-26 13:10:58 +0200

    reading list of students from file and sorting them.

--- src/main/java/com/lits/maven/sample/App.java
+++ src/main/java/com/lits/maven/sample/App.java
@@ -1,11 +1,41 @@
 package com.lits.maven.sample;
 
-/**
- * Hello world!
- *
- */
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.lits.maven.sample.Utility;
+
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Test!");
+		List<Students> studentsList = null;
+		try {
+			studentsList = Utility.parseJsonFile();
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		if (studentsList != null) {
+			System.out.println("\nStudens list: \n");
+			for (Students item : studentsList) {
+				System.out.println(item.getSurname() + " " + item.getAge() + " " + item.getYear());
+			}
+			Collections.sort(studentsList, Students.StuNameComparator);
+			System.out.println("\nSorted list of students: \n");
+			for (Students item : studentsList) {
+				System.out.println(item.getSurname() + " " + item.getAge() + " " + item.getYear());
+			}
+		} else {
+			System.out.println("Students list is empty");
+		}
 	}
 }
